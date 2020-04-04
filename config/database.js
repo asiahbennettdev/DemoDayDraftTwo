c2cfa42c38daef9131ea8409d3e7b2e0f7f03bed
Asiah Bennett Dev <asiahbennettdev@gmail.com>
2020-04-03 22:18:55 -0400

    demo day raft two
    Demo Day Views  HTML CSS

---
+++ config/database.js
@@ -0,0 +1,6 @@
+// config/database.js
+module.exports = {
+
+    'url' : 'mongodb+srv://DemoDay:draft@demo-day-draft-one-0fps0.mongodb.net/test?retryWrites=true',
+    'dbName': 'Demo-Day-Draft-One'
+};
